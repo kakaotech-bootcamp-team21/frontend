7ecc23f374c09b3ea6b17f3c3cef5d4ecfb2e9c2
yoonseopkim <qc70045@gmail.com>
2024-08-10 13:13:07 +0900

    Develop (#9)
    * [FEAT] 메인 페이지 틀 생성
    
    * [FEAT] 페이지전환 및 사용자 상태에 따라 로그인 버튼 렌더링 하도록 함
    
    * README.md
    
    * ivy 꺼랑 합침
    
    ---------
    
    Co-authored-by: dayaelee <dayae.dev@gmail.com>

---
+++ src/Pages/MyPage.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../components/ui/Button";
+import { handleLogin, handleLogout, getUserType } from '../utils/auth';
+
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ButtonWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 16px; // 버튼 아래 여백 추가
+`;
+
+
+function MyPage(props) {
+    const [userType, setUserType] = useState(getUserType());
+    const navigate = useNavigate();
+
+    return (
+        <Wrapper>
+            <ButtonWrapper>
+                {userType === 'regular' ? (
+                    <>
+                        <Button
+                            title="로그아웃" 
+                            onClick={() => {
+                                handleLogout(setUserType);
+                                navigate("/");
+                            }}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Button
+                            title="로그아웃"
+                            onClick={handleLogout(setUserType)}
+                        />
+                    </>
+                )}
+            </ButtonWrapper>
+            <br/>
+            <br/>
+            <p>마이 페이지입니다. </p>
+        </Wrapper>
+    );
+
+}
+
+export default MyPage;
