6efb730781c6cc131df69e4f8a84901fe1df4848
yoonseopkim <qc70045@gmail.com>
2024-08-13 23:11:20 +0900

    화상채팅 기능및 라우팅 추가 /video-chat

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import AI_Main from "./pages/AI_Main";
 import Mypage from './pages/Mypage';
 import InfoPage from './pages/InfoPage';
 import SubmitPage from './pages/SubmitPage';
+import VideoChat from "./pages/VideoChat";
 const MainTitleText = styled.p`
     font-size: 24px;
     font-weight: bold;
@@ -102,6 +103,7 @@ function App(props) {
               <Route path="/submit" element={<SubmitPage />} />
               <Route path="/Mypage" element={<Mypage />} />
               <Route path="/ai-main" element={<AI_Main />} />
+              <Route path="/video-chat" element={<VideoChat />} />
           </Routes>
      </div>
   );
