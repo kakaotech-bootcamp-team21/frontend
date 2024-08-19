561c3dac735a74de439565867c04035bd93dddbd
yoonseopkim <qc70045@gmail.com>
2024-08-19 19:57:24 +0900

    feature/main css 오류 해결

--- src/components/navbars/AiNavbar.js
+++ src/components/navbars/AiNavbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import '../../css_ai/AIMain.css';
+import './MainNavbar.css'
 import '../../App.css';
 import InfoPage from '../../pages/ai/InfoPage';
 import SubmitPage from '../../pages/ai/SubmitPage';
@@ -12,80 +12,78 @@ function AI_Navbar() {
     const [active, setActive] = useState('');
     const [showSubMenu, setShowSubMenu] = useState(false);
     const [subMenuStyle, setSubMenuStyle] = useState({});
-    const menuRef = useRef(null);  // 메인 메뉴 아이템에 대한 ref
-  
+    const menuRef = useRef(null);
+
     const menus = ['전문가에게 첨삭요청', 'AI 기반 자소서 첨삭하기', '합격자소서 모음집', '첨삭 요청 현황'];
-  
+
     const handleMenuMouseOver = (menu) => {
-      setActive(menu);
-      if (menu === 'AI 기반 자소서 첨삭하기' && menuRef.current) {
-          setShowSubMenu(true);
-          const rect = menuRef.current.getBoundingClientRect();
-          setSubMenuStyle({
-              top: `${rect.bottom}px`,
-              left: `${rect.left}px`,
-              width: `${rect.width}px`
-          });
-      } else {
-          setShowSubMenu(false);
-      }
-  };
-  
+        setActive(menu);
+        if (menu === 'AI 기반 자소서 첨삭하기' && menuRef.current) {
+            setShowSubMenu(true);
+            const rect = menuRef.current.getBoundingClientRect();
+            setSubMenuStyle({
+                top: `${rect.bottom}px`,
+                left: `${rect.left}px`,
+                width: `${rect.width}px`
+            });
+        } else {
+            setShowSubMenu(false);
+        }
+    };
+
     const handleMenuMouseOut = () => {
-      setShowSubMenu(false);
+        setShowSubMenu(false);
     };
     const handleSubMenuMouseOver = () => {
-      setShowSubMenu(true);
+        setShowSubMenu(true);
     };
-  
+
     const handleSubMenuMouseOut = () => {
-      setShowSubMenu(false);
+        setShowSubMenu(false);
     };
 
     const navigate = useNavigate();
 
     const handleMenuClick = (menu) => {
-      if (menu === '전문가에게 첨삭요청') {
-        navigate('/request-expert');
-      } else if (menu === '합격자소서 모음집'){
-
-      } else if (menu === '첨삭 요청 현황'){
-        navigate('/request-status');
-
-      }
-
+        if (menu === '전문가에게 첨삭요청') {
+            navigate('/request-expert');
+        } else if (menu === '합격자소서 모음집'){
+            // 해당 페이지로 이동하는 로직 추가
+        } else if (menu === '첨삭 요청 현황'){
+            navigate('/request-status');
+        }
     }
-  
-    const subMenus = [
-      { name: '주요 정보 입력', path: '/info' },
-      { name: '자기소개서 제출', path: '/submit' }
-    ];
-  
-    return (
-      <nav className="navbar">
-        {menus.map((menu, index) => (
-          <button
-            key={menu}
-            ref={menu === 'AI 기반 자소서 첨삭하기' ? menuRef : null}
-            className={`nav-button ${active === menu ? 'active' : ''}`}
-            onMouseOver={() => handleMenuMouseOver(menu)}
-            onMouseOut={handleMenuMouseOut}
-            onClick={() => handleMenuClick(menu)}
-          >
-            {menu}
-          </button>
-        ))}
-        {showSubMenu && (
-          <div className="sub-menu" style={subMenuStyle} onMouseOver={handleSubMenuMouseOver} onMouseOut={handleSubMenuMouseOut}>
-            {subMenus.map(subMenu => (
-              <Link to={subMenu.path} key={subMenu.name} className="nav-button">
-                {subMenu.name}
-              </Link>
-            ))}
-          </div>
-        )}
-      </nav>
-    );
-  }
 
-  export default AI_Navbar;
+    const subMenus = [
+        { name: '주요 정보 입력', path: '/info' },
+        { name: '자기소개서 제출', path: '/submit' }
+    ];
+
+    return (
+        <nav className="main-nav-bar-navbar">
+            {menus.map((menu, index) => (
+                <button
+                    key={menu}
+                    ref={menu === 'AI 기반 자소서 첨삭하기' ? menuRef : null}
+                    className={`main-nav-bar-nav-button ${active === menu ? 'active' : ''}`}
+                    onMouseOver={() => handleMenuMouseOver(menu)}
+                    onMouseOut={handleMenuMouseOut}
+                    onClick={() => handleMenuClick(menu)}
+                >
+                    {menu}
+                </button>
+            ))}
+            {showSubMenu && (
+                <div className="main-nav-bar-sub-menu" style={subMenuStyle} onMouseOver={handleSubMenuMouseOver} onMouseOut={handleSubMenuMouseOut}>
+                    {subMenus.map(subMenu => (
+                        <Link to={subMenu.path} key={subMenu.name} className="main-nav-bar-nav-button">
+                            {subMenu.name}
+                        </Link>
+                    ))}
+                </div>
+            )}
+        </nav>
+    );
+}
+
+export default AI_Navbar;
